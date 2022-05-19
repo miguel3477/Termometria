636701f845e51fab59b8154d6800716d64addf3a
Marcos <43245837+miguel3477@users.noreply.github.com>
2022-05-19 13:11:52 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,19 +5,41 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <title>Termometria</title>
     <script>
+        function light() {
+            localStorage.setItem ('theme', 'light');
+            myFunction();
+        }
+
+        function dark() {
+            localStorage.setItem ('theme', 'dark');
+            myFunction();
+        }
+
         function myFunction() {
-            var element = document.body;
-            element.classList.toggle("light-mode");
-            
-            document.getElementById("header").classList.toggle("light-mode-header");
-            document.getElementById("content").classList.toggle("light-mode-content");
-            document.getElementById("bottom").classList.toggle("light-mode-bottom");
+            var userMode = localStorage.getItem ('theme');
+
+            if (userMode == 'light') {
+                document.getElementsByTagName("BODY")[0].style.backgroundColor = "white";
+
+                document.getElementById("header").style.backgroundColor = "#7B0F28";
+                document.getElementById("content").style.backgroundColor = "#EC6988";
+                document.getElementById("bottom").style.backgroundColor = "#D61A46";
+                document.getElementById("bottom").style.borderColor = "#D61A46";
+            }
+            else if (userMode == 'dark') {
+                document.getElementsByTagName("BODY")[0].style.backgroundColor = "#000000e1";
+
+                document.getElementById("header").style.backgroundColor = "#600073";
+                document.getElementById("content").style.backgroundColor = "blueviolet";
+                document.getElementById("bottom").style.backgroundColor = "#aa00cc";
+                document.getElementById("bottom").style.borderColor = "#aa00cc";
+            }
         }
     </script>
-    <title>Termometria</title>
 </head>
-<body>
+<body class="dark-theme" onload="myFunction()">
     <div class='container'>
     <form method="post" action="">
         <div class='caixa-header' id='header'><h1>Conversor de Temperatura</h1></div>
@@ -41,9 +63,8 @@
             
         <input class='caixa-bottom' id='bottom' type="submit" value="Converter"><br>
         </form>
-        
-        <button onclick="myFunction()">Light Theme</button> 
-
+                  
     </div>
+    <div><button id="light" onclick="light()"></button><button id="dark" onclick="dark()"></button></div>
 </body>
 </html>
